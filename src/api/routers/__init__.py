0a38992c9f97fa2124674ce0c96bba78d265ad1d
hino-s1310 <hisatom0907@gmail.com>
2025-08-31 17:26:34 +0900

    feat: ルーティング層の分離 - routers/files.pyにファイル操作関連のルーティングを追加 - 上記のルーターに対するunitテストを追加

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -5,6 +5,5 @@ FastAPIのルーター定義を管理
 """
 
 from .files import router as files_router
-from .system import router as system_router
 
-__all__ = ["files_router", "system_router"]
+__all__ = ["files_router"]
